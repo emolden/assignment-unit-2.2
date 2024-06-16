1345f34391d75720abc103e67ab03f084c48564c
Emma Molden <emma.molden007@gmail.com>
2024-06-16 15:10:26 -0500

    finished 4 through 6

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -30,7 +30,7 @@ if (number >= 2) {
 // We check if the name variable is "Mary". 
 // If the name variable is "Mary", we console.log "Hi, Mary!"
 // If the name variable is not "Mary", we console.log "How do you do?"
-//
+// The output is "How do you do?"
 
 //CODE
 /*
@@ -51,9 +51,11 @@ if (name === 'Mary') {
 // We check if the variable code is equal to 123.
 // If code is equal to 123, we assign "secret" to string "super" and mutliply 123 (code) by 2.
 // If code is not equal to 123, no change.
+// The variable secret is now assigned string "super".
+// The variable code is now assigned number 246.
 // We check if the variable code is greater than 250.
 // If code is greater than 250, we assign the variable "secret" to string "duper".
-// If code is not greater than 250, no change.
+// If code is not greater than 250, no change. There is not change.
 // We console.log secret. The output will be "super". 
 
 //CODE
@@ -81,12 +83,17 @@ console.log(secret)
 // We create new variable "zip" and assign number "55407".
 // We check if isStudent is exactly equal to true AND if zip is greater than 80000.
 // If both isStudent is true AND zip is greater than 80000, we console.log "You're a student on the West Coast!".
+// There is no output.
 // We check if isStudent is exactly equal to false OR if age is less than 30.
 // If isStudent is false OR if age is less than 30, we console.log "What are your hobbies?"
+// There is no output.
 // We check if isStudent is exactly equal to true.
 // If isStudent is true, we console.log "Welcome to Prime!"
+// There is an output "Welcome to Prime!"
 // If none of the above options are true (which I think is impossible because isStudent has the 
 // option of being true or false), we console.log "How about the weather?"
+// There is no additional output.
+
 
 //CODE
 /*
@@ -143,10 +150,13 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+// FIX colorOne is set to red but should be set to blue. let colorOne = 'blue';
+// colorTwo is set to blue but should be set to red. let colorTwo = 'red';
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
 
+//FIX colorTwo should also be reassigned to 'purple'. Under colorOne = 'purple'; should be colorTwo = 'purple';
 if (mix === true) {
   colorOne = 'purple';
 }
@@ -157,9 +167,12 @@ if (mix === true) {
 // -- they are so we console.log 'throw away the food!'
 
 /*
+//FIX time is a constant but should be avariable. let time = 4;
 let temp = 40;
 const time = 4;
 
+//FIX The two conditionals have an OR between them but they should have AND.
+// temp > 39 && time >= 4
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
 }
@@ -170,9 +183,14 @@ if (temp > 39 || time >= 4) {
 // -- it is, so we console.log 'enter'.
 
 /*
+//FIX minAge should be a variable but it is a constant.
+// let minAge = 21;
 let age = 21;
 const minAge = 21;
 
+//MAYBE FIX the conditional is backwards to the description. I don't think this should effect the output
+// but to match the descrition it should be age >= minAGE
+//FIX the console.log('enter'); should be after the if statement.
 if(minAge <= age) {
   console.log('no entry');
 } else {
